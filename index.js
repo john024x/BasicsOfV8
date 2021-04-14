e2ed42de06d15f733f824c47b4d668fab9fdb06b
Juan Fuentes <juan@2806-106e-0023-b91b-0000-0000-0000-000f.ipv6.infinitum.net.mx>
2021-04-13 23:18:16 -0500

    Avoiding the hoisting

--- index.js
+++ index.js
@@ -8,3 +8,50 @@ function apellidos(){
 //y por ultimo imprimirlas
 console.log(nombre);
 apellidos();
+
+//codigo de ejecución ---> Memory Heap
+//single thread (synchronous)
+var nombre = "Juan" //en este momento se destina un espacio en memoria para guardar este valor
+const edad = 22;
+const carro = {
+    marca: "Toyota",
+    modelo: "2021",
+}
+
+//call stack(probar en navegador)
+function restaDos(num){
+    return num-2;
+}
+function calcular(){
+    const sumarTotal = 4+5;
+    return restaDos(sumarTotal);
+}
+debbuger;
+calcular();
+
+//garbage Collection
+//mark and sweep
+
+//stackoverflow
+//viene de el callstack, donde se van apilando las tareas (cuando este se desborda) se crashea el navegador
+/*
+function overflow(){
+  overflow();
+}
+overflow();
+*/
+
+//Javascript runtime
+console.log("taco 1");
+console.log("taco 2");
+console.log("tortita");
+console.log("taco 3");
+
+//asincronismo
+//javascript corre una tarea a la vez
+console.log("taco 1");
+console.log("taco 2");
+setTimeout(() =>{
+    console.log("tortita");
+},0);//con el 0 de tiempo de espera, aun asi primero termina todas las otras tareas y despues ejecuta estas tareas
+console.log("taco 3");
